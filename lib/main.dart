d83ae9a83136b7ee3856485d34eaf6f95cb74698
Muhammad Vaid <molvy32@gmail.com>
2022-06-10 22:11:14 +0200

    HookWidget with useEffect

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MyApp());
@@ -58,6 +59,30 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Text(_number.toString()),
+      ),
+    );
+  }
+}
+
+class HomePageHook extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final numberNotifier = useState(0);
+    useEffect(() {
+      // Use as if init state
+      final timer = Timer.periodic(const Duration(seconds: 1), (Timer timer) {
+        numberNotifier.value = timer.tick;
+      });
+      return () {
+        timer.cancel();
+      };
+    }, []);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Center(
+        child: Text(numberNotifier.value.toString()),
       ),
     );
   }
